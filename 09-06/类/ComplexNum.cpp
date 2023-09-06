b0542fb1c1691d59fc412c7e4c770ae212634946
BertreB <84637248+RoweMann@users.noreply.github.com>
2023-09-06 17:08:51 +0800

    测试

--- 09-06/类/ComplexNum.cpp
+++ 09-06/类/ComplexNum.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "ComplexNum.h"
+#include <iostream>
+#include <cmath>
+int main( )
+{
+    Complex c1(5.0,10.0);   	//5+10i
+    Complex c2(3.0,-2.5);    	//3-2.5i
+    Complex c3,c4;
+    c3=c1+c2;
+    c4=c1-5;
+    cout<<c1<<c2<<c3<<c4;
+    Complex c5;
+    cout<<"请输入一个复数,实部和虚部以空格分开:"<<endl;
+    cin>>c5;
+    cout<<"输入的复数为:"<<c5<<endl;
+ c4=5+c5;
+
+    cout<<c4<<endl;
+    return 0;
+}
--- 09-06/类/ComplexNum.h
+++ 09-06/类/ComplexNum.h
@@ -1,16 +1,60 @@
-//
-// Created by Yiming Wang on 2023/9/6.
-//
-
-#ifndef INC_09_06_COMPLEXNUM_H
-#define INC_09_06_COMPLEXNUM_H
+#include <iostream>
+#include <cmath>
 
 
 
-class ComplexNum {
+class Complex
+{
+public:
+    Complex()
+    {
+        Re=0;
+        Im=0;
+    }
 
+    Complex(double Re, double Im)
+    {
+        re = Re;
+        im = Im;
+    }
+
+    Complex(double Re)
+    {
+        re = Re;
+        im = 0;
+    }
+
+    friend Complex operator+(const Complex c1, const Complex c2);
+    friend Complex operator-(const Complex c1, const Complex c2);
+
+    
+    friend ostream &operator<<(stream &outs, const Complex &B);
+    friend istream &operator>>(stream &ins, const Complex &C);
+    {
+        ins>>C.Re>>C.Im;
+    }
+
+private:
+    double re;
+    double im;
+    double x;
 };
 
 
+Complex operator+(const Complex c1, const Complex c2)
+{
+    return Complex(c1.Re+c2.Re,c1,Im+c2.Im)
+}
+Complex operator-(const Complex c1, const Complex c2);
+{
+    return Complex(c1.Re-c2.Re,c1,Im-c2.Im)
+}
 
-#endif //INC_09_06_COMPLEXNUM_H
+ostream &operator<<(ostream&outs,const Complex &B)
+{
+if(B.imag>0)
+    outs<<"("<<B.real<<","<<B.imag<<"i)"<<"\n";
+    else
+         outs<<"("<<B.real<<","<<B.imag<<"i)"<<"\n";
+}
+
